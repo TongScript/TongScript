6da8649f89f72d1ebef7f471fb0f9682944d0a90
Pluto <158065504+TongScriptX@users.noreply.github.com>
2024-09-29 19:24:34 +0800

    Update TXgames.lua

--- TXgames.lua
+++ TXgames.lua
@@ -2,6 +2,7 @@ local Games = {
     [4618049391] = "TangCountyHebei.lua",
     [3623096087] = "MuscleLegends.lua",
     [14475792710] = "DealershipTycoon.lua",
+    [891852901] = "GreenVille.lua",
     [1] = "universal.lua",
 }
 
@@ -11,31 +12,4 @@ local script = function()
     return game:HttpGet(repo..scriptName)
 end
 
-
-local function showNotification(gameName)
-    local NotificationHolder =
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-    local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
-
-    Notification:Notify(
-        {Title = "TongScript X", Description = "是否加载"..gameName.. "脚本"},
-        {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 999, Type = "option"},
-        {
-            Image = "http://www.roblox.com/asset/?id=6023426923",
-            ImageColor = Color3.fromRGB(255, 84, 84),
-            Callback = function(State)
-                if State then
-                    wait(1)
-                    loadstring(script())()
-                else
-                    print(tostring(State))
-                end
-            end
-        }
-    )
-end
-
-
-local detectedGameName = Games[game.PlaceId] or "universal.lua"
-detectedGameName = string.gsub(detectedGameName, ".lua", "")
-showNotification(detectedGameName)
+loadstring(script())()
